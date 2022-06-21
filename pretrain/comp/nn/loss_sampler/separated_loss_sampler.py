a896df1e306e7e3773361b972d30fe6085a3990d
Asichurter <asichurter@gmail.com>
2022-06-21 22:08:32 +0800

    update separated loss sampler

--- pretrain/comp/nn/loss_sampler/separated_loss_sampler.py
+++ pretrain/comp/nn/loss_sampler/separated_loss_sampler.py
@@ -3,6 +3,8 @@ from typing import Dict
 import torch
 
 from common.nn.loss_func import LossFunc
+from pretrain.comp.nn.loss_sampler.common_utils import stat_true_count_in_batch_dim, \
+    sample_2D_mask_by_count_in_batch_dim
 from pretrain.comp.nn.loss_sampler.loss_sampler import LossSampler
 
 
@@ -21,18 +23,16 @@ class SeparatedFullLossSampler(LossSampler):
         :param vertice_num: Shape: [batch, 1]
         :return:
         """
-        assert edge_matrix.shape == predicted_matrix.shape[:3], "Unmatched shape between label edges and predicted edges"
-        assert predicted_matrix.size(1) == 2, "The second dimension should be 2 for data and control predictions"
         # Drop 0-th row and column, since line index starts from 1.
         edge_matrix = edge_matrix[:, :, 1:, 1:]
         # Minus one to make label range [0,1].
         edge_matrix -= 1
+        assert edge_matrix.shape == predicted_matrix.shape[:3], "Unmatched shape between label edges and predicted edges"
+        assert predicted_matrix.size(1) == 2, "The second dimension should be 2 for data and control predictions"
 
-        loss_mask = (edge_matrix != -1).float()
-        # Set reduction to 'none' to keep dimensions for masking padded pairs.
-        loss_matrix = self.loss_func(predicted_matrix, edge_matrix, reduction='none')
-        loss = (loss_matrix * loss_mask).sum() / loss_mask.sum()
-        return loss
+        # Manually operating "masked_mean"
+        loss_mask = (edge_matrix != -1).int()
+        return self.cal_matrix_masked_loss_mean(predicted_matrix, edge_matrix, loss_mask)
 
 
 @LossSampler.register('separated_balanced')
@@ -48,41 +48,24 @@ class SeparatedBalancedLossSampler(LossSampler):
                  edge_matrix: torch.Tensor,
                  predicted_matrix: torch.Tensor,
                  vertice_num: torch.Tensor) -> torch.Tensor:
-        assert edge_matrix.shape == predicted_matrix.shape, "Unmatched shape between label edges and predicted edges"
-        assert predicted_matrix.size(1) == 2, "The second dimension should be 2 for data and control predictions"
         # Drop 0-th row and column, since line index starts from 1.
         edge_matrix = edge_matrix[:, :, 1:, 1:]
         # Minus one to make label range [0,1].
         edge_matrix -= 1
+        assert edge_matrix.shape == predicted_matrix.shape, "Unmatched shape between label edges and predicted edges"
+        assert predicted_matrix.size(1) == 2, "The second dimension should be 2 for data and control predictions"
 
         bsz, _, v_num = edge_matrix.shape[:3]
-        edge_matrix = edge_matrix.view(bsz*2, v_num*v_num)
+        # Here we must use .reshape instead of .view
+        edge_matrix = edge_matrix.reshape(bsz*2, v_num*v_num)
+        predicted_matrix = predicted_matrix.reshape(bsz*2, v_num*v_num)
 
         # Get indexes for edge/non-edge positions.
         # Note non-edged positions have index 0.
-        no_edge_idx_list = (edge_matrix == 0).nonzero()
+        non_edge_mask = (edge_matrix == 0)
         edge_mask = (edge_matrix == 1)
-        sampled_mask = edge_mask
 
-        # Count number of edges per instance in the batch.
-        zeros_to_be_filled = torch.zeros((bsz*2), dtype=torch.int, device=edge_matrix.device)
-        ones_to_fill = torch.ones_like(no_edge_idx_list[:,0], dtype=torch.int, device=edge_matrix.device)
-        edge_count = torch.scatter_add(zeros_to_be_filled, 0, no_edge_idx_list[:,0], ones_to_fill)
-
-        # Sample the same number of non-edged pair as the edged pair.
-        # This operation can not be easily implemented in a batched manner, thus
-        # sampling is done iteratively in the batch.
-        sampled_non_edge_idxes = []
-        for i in range(2*bsz):
-            edge_count_i = int(edge_count[i].item())
-            # Selected indexes are in 1D form, reshape to 2D.
-            i_non_edge_idxes = torch.masked_select(no_edge_idx_list, no_edge_idx_list[:,0:1]==i).view(-1, 2)
-            sampled_i_non_edge_idxes = i_non_edge_idxes[torch.randperm(i_non_edge_idxes.size(0))[:edge_count_i]]
-            sampled_non_edge_idxes.append(sampled_i_non_edge_idxes)
-
-        # Set sampled non-edged positions to be True,
-        # and mask the labels of False positions to be -1.
-        sampled_non_edge_idxes = torch.stack(sampled_non_edge_idxes, dim=0)
-        sampled_mask[sampled_non_edge_idxes[:,0], sampled_non_edge_idxes[:,1]] = True
-        edge_matrix.masked_fill_(~sampled_mask, -1)
-        return self.loss_func(predicted_matrix, edge_matrix)
+        edge_count = stat_true_count_in_batch_dim(edge_mask)
+        sampled_non_edge_mask = sample_2D_mask_by_count_in_batch_dim(non_edge_mask, edge_count)
+        sampled_mask = sampled_non_edge_mask.bool() | edge_mask
+        return self.cal_matrix_masked_loss_mean(predicted_matrix, edge_matrix, sampled_mask)
