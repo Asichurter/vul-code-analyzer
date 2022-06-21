4726855e3ac804cd9cb632e040da9c4120b44230
Asichurter <asichurter@gmail.com>
2022-06-21 19:41:16 +0800

    fix multi-dimension input of nll loss func

--- common/nn/loss_func.py
+++ common/nn/loss_func.py
@@ -1,5 +1,6 @@
 import torch
 from torch.nn import functional
+from functools import reduce
 
 from allennlp.common import Registrable
 
@@ -40,19 +41,37 @@ class BinaryCrossEntropyLoss(LossFunc):
 
 @LossFunc.register('cross_entropy')
 class CrossEntropyLoss(LossFunc):
-    def forward(self, pred: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
-        return functional.cross_entropy(pred, label, ignore_index=self.ignore_index)
+    def forward(self, pred: torch.Tensor, label: torch.Tensor, **kwargs) -> torch.Tensor:
+        return functional.cross_entropy(pred, label, ignore_index=self.ignore_index, **kwargs)
 
 
 @LossFunc.register('nll')
 class NllLoss(LossFunc):
-    def forward(self, pred: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
-        return functional.nll_loss(pred, label, ignore_index=self.ignore_index)
+    def forward(self, pred: torch.Tensor, label: torch.Tensor, **kwargs) -> torch.Tensor:
+        pred_shape = pred.shape
+        label_shape = label.shape
+
+        # Adapt multi-dimensional pred/label tensor.
+        if len(pred.shape) > 2:
+            dummy_pred_batch_size = reduce(lambda x,y: x*y, pred_shape[:-1], 1)
+            dummy_label_batch_size = reduce(lambda x,y: x*y, label_shape, 1)
+            assert dummy_pred_batch_size==dummy_pred_batch_size, f'{dummy_pred_batch_size}!={dummy_pred_batch_size}'
+            pred = pred.reshape((dummy_pred_batch_size, pred_shape[-1]))
+            label = label.reshape(dummy_label_batch_size,)
+
+        loss = functional.nll_loss(pred, label, ignore_index=self.ignore_index, **kwargs)
+
+        # Adapt dimension keeping.
+        if 'reduction' in kwargs and kwargs['reduction'] == 'none':
+            return loss.reshape(label_shape)
+        else:
+            return loss
+
 
 
 @LossFunc.register('mean_square')
 class MeanSquareLoss(LossFunc):
-    def forward(self, pred: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
+    def forward(self, pred: torch.Tensor, label: torch.Tensor, **kwargs) -> torch.Tensor:
         pred = pred.squeeze()
         assert pred.size() == label.size(), f'MSE assumes logit and label have the same size,' \
                                              f'but got {pred.size()} and {label.size()}'
@@ -66,10 +85,10 @@ class BCEWithLogitsLoss(LossFunc):
         super().__init__()
         self._loss = torch.nn.BCEWithLogitsLoss()
 
-    def forward(self, pred: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
+    def forward(self, pred: torch.Tensor, label: torch.Tensor, **kwargs) -> torch.Tensor:
         # align batch_size of predictions and labels in tail batch
         pred, label = self.align_pred_label_batch_size(pred, label)
-        return self._loss(pred, label)
+        return self._loss(pred, label, **kwargs)
 
 
 @LossFunc.register('bce')
@@ -78,9 +97,9 @@ class BCELoss(LossFunc):
         super().__init__()
         self._loss = torch.nn.BCELoss()
 
-    def forward(self, pred: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
+    def forward(self, pred: torch.Tensor, label: torch.Tensor, **kwargs) -> torch.Tensor:
         # align batch_size of predictions and labels in tail batch
         pred, label = self.align_pred_label_batch_size(pred, label)
         pred = pred.view(pred.size(0),)
         label = label.view(pred.size(0),)
-        return self._loss(pred, label.float())  # float type tensor is expected for 'label'
+        return self._loss(pred, label.float(), **kwargs)  # float type tensor is expected for 'label'
