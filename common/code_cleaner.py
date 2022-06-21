f9240f35c70248a8ba734493cb3af7229c30079e
Asichurter <asichurter@gmail.com>
2022-06-21 22:07:09 +0800

    add missing \n at last line in code cleaner

--- common/code_cleaner.py
+++ common/code_cleaner.py
@@ -4,6 +4,12 @@ from allennlp.common.registrable import Registrable
 
 
 class CodeCleaner(Registrable):
+    def basic_code_process(self, code: str):
+        # Add missing new_line char to last line.
+        if code[-1] != '\n':
+            code += '\n'
+        return code
+
     def clean_code(self, code: str) -> str:
         raise NotImplementedError
 
@@ -12,6 +18,7 @@ class CodeCleaner(Registrable):
 @CodeCleaner.register('default')
 class TrivialCodeCleaner(CodeCleaner):
     def clean_code(self, code: str) -> str:
+        code = self.basic_code_process(code)
         return code
 
 
@@ -21,5 +28,7 @@ class SpaceSubCodeCleaner(CodeCleaner):
     Replace multiple spaces and tabs(\s) with a single space.
     """
     def clean_code(self, code: str) -> str:
+        code = self.basic_code_process(code)
         # return re.sub(r'^\s+|\s+$|\s+(?=\s)', ' ', code)
-        return re.sub(r' +|\t+', ' ', code)
+        code = re.sub(r' +|\t+', ' ', code)
+        return code
