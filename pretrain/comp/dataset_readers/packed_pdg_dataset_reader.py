8e5225d9b94257f21fb3e7fe49a9022e532bc5b1
Asichurter <asichurter@gmail.com>
2022-06-21 22:07:51 +0800

    add data checking and separated edge label type

--- pretrain/comp/dataset_readers/packed_pdg_dataset_reader.py
+++ pretrain/comp/dataset_readers/packed_pdg_dataset_reader.py
@@ -25,6 +25,7 @@ class PackedLinePDGDatasetReader(DatasetReader):
                  tokenized_newline_char: str = 'Ċ',  # \n after tokenization by CodeBERT
                  special_tokenizer_token_handler_type: str = 'codebert',
                  only_keep_complete_lines: bool = True,
+                 unified_label: bool = True,
                  **kwargs):
         super().__init__(**kwargs)
         self.code_tokenizer = code_tokenizer
@@ -39,6 +40,9 @@ class PackedLinePDGDatasetReader(DatasetReader):
         # self.skip_first_token_line_index = skip_first_token_line_index
         self.special_tokenizer_token_handler_type = special_tokenizer_token_handler_type
         self.only_keep_complete_lines = only_keep_complete_lines
+        self.unified_label = unified_label
+
+        self.actual_read_samples = 0
 
 
     def make_edge_matrix(self,
@@ -56,15 +60,25 @@ class PackedLinePDGDatasetReader(DatasetReader):
         :return:
         """
         # To cover the last line (line_count-th), we have to allocate one more line here.
-        matrix = torch.zeros((line_count+1, line_count+1), dtype=torch.long)
-        matrix += 4       # default no dependency
+        if self.unified_label:
+            matrix = torch.zeros((line_count+1, line_count+1), dtype=torch.long)
+            matrix += 4  # default no dependency
+        else:
+            matrix = torch.zeros((2, line_count+1, line_count+1))
+            matrix += 1  # default no dependency
         for edge in edges:
             tail, head, etype = re.split(',| ', edge)   # tail/head vertice index start from 1 instead of 0
             tail, head, etype = int(tail), int(head), int(etype)
             # Ignore uncovered vertices (lines)
             if tail > line_count or head > line_count:
                 continue
-            matrix[tail, head] = etype
+            if self.unified_label:
+                matrix[tail, head] = etype
+            else:
+                if etype == 3 or etype == 1:
+                    matrix[0, tail, head] = 2   # 2 refers to edge
+                if etype == 3 or etype == 2:
+                    matrix[1, tail, head] = 2
         return matrix
 
 
@@ -111,26 +125,50 @@ class PackedLinePDGDatasetReader(DatasetReader):
                 break
 
         if self.only_keep_complete_lines:
-            # FixBug: Empty tokens when 'current_column' is 0.
-            if current_column > 0:
+            # FixBug: Empty tokens when 'current_column' is 0 or 'current_line' is 1.
+            if current_column > 0 and current_line > 1:
                 line_tokens = line_tokens[:-current_column]
                 line_idxes = line_idxes[:-current_column]
 
         line_tokens, line_idxes = self.post_handle_special_tokenizer_tokens(line_tokens, line_idxes)
         return line_tokens, torch.LongTensor(line_idxes), current_line-1
 
+    def _check_data_correctness(self,
+                                original_code: str,
+                                tokenized_code: List[Token],
+                                line_indexes: torch.Tensor,
+                                edge_matrix: torch.Tensor):
+        from utils import GlobalLogger as mylogger
+        # 1. Check tokenized code
+        if self.special_tokenizer_token_handler_type == 'codebert':
+            if len(tokenized_code) == 2:
+                mylogger.error('_check_data_correctness',
+                               f'Found empty tokenized code, original code: {original_code}')
+        else:
+            mylogger.warning('_check_data_correctness', f'Unhandled tokenized type: {self.special_tokenizer_token_handler_type}')
 
-    def text_to_instance(self, packed_pdg: Dict) -> Instance:
+        # 2. Check consistency between code and line index
+        if self.special_tokenizer_token_handler_type == 'codebert':
+            if len(tokenized_code) - 2 != len(line_indexes):
+                mylogger.error('_check_data_correctness',
+                               f'Inconsistency found between line index and tokenized code: ' +
+                               f'code_len({len(tokenized_code)}) - 2 != index_len({len(line_indexes)}). '
+                               f'\noriginal code: {original_code}')
+
+    def text_to_instance(self, packed_pdg: Dict) -> Tuple[bool, Instance]:
         code = packed_pdg['code']
         edges = packed_pdg['edges']
         original_total_line = packed_pdg['total_line']
 
         code = self.code_cleaner.clean_code(code)
         tokenized_code = self.code_tokenizer.tokenize(code)
-        # tokenized_code = self.drop_tokenizer_special_tokens(tokenized_code)
         tokenized_code, token_line_idxes, line_count = self.truncate_and_make_line_index(tokenized_code)
         edge_matrix = self.make_edge_matrix(edges, line_count)
+        # Ignore single-line code samples.
+        if line_count == 1:
+            return False, Instance({})
 
+        self._check_data_correctness(code, tokenized_code, token_line_idxes, edges)
         fields = {
             'code': TextField(tokenized_code, self.code_token_indexers),
             'line_idxes': TensorField(token_line_idxes),
@@ -138,7 +176,7 @@ class PackedLinePDGDatasetReader(DatasetReader):
             'vertice_num': TensorField(torch.Tensor([line_count])), # num. of line is vertice num.
         }
 
-        return Instance(fields)
+        return True, Instance(fields)
 
 
     def _read(self, data_base_path: str) -> Iterable[Instance]:
@@ -150,6 +188,9 @@ class PackedLinePDGDatasetReader(DatasetReader):
             for item in tqdm(os.listdir(vol_path)):
                 try:
                     pdg_data_item = read_dumped(os.path.join(vol_path, item))
-                    yield self.text_to_instance(pdg_data_item)
+                    ok, instance = self.text_to_instance(pdg_data_item)
+                    if ok:
+                        self.actual_read_samples += 1
+                        yield instance
                 except Exception as e:
                     logger.error('read', f'file path: {os.path.join(vol_path, item)}, error: {str(e)}')
