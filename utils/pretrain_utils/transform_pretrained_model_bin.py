b884c158ca58414036d67234a7d7f1ba734f6370
Asichurter <asichurter@gmail.com>
2022-09-19 11:56:45 +0800

    add transform util of pre-trained model package

---
+++ utils/pretrain_utils/transform_pretrained_model_bin.py
@@ -0,0 +1,38 @@
+"""
+    This script is used to transform the pre-trained model state dict into pytorch_model.bin
+     of the Transformers model package, which can be directly used via "Transformers.Auto" or
+     AllenNLP.
+"""
+
+
+import torch
+
+weight_path = '/data1/zhijietang/vul_data/run_logs/pretrain/25/state_epoch_9.th'
+extra_weight_path = '/data1/zhijietang/vul_data/transformers_repos/codebert-mlm/pytorch_model.bin'
+pretrained_weights_prefix = 'code_embedder.token_embedder_code_tokens.transformer_model.'
+real_vocab_size = 50265
+vocab_embedding_key = 'embeddings.word_embeddings.weight'
+target_dump_path = '/data1/zhijietang/vul_data/transformers_repos/codebert-mlm-full-basicflt/pytorch_model.bin'
+extra_weights_need_to_be_add = ['pooler.dense.weight', 'pooler.dense.bias']
+
+model_to_be_adapt = torch.load(weight_path)
+extra_reference_model = torch.load(extra_weight_path)
+new_state_dict = {}
+
+# Step 1: Load weights by truncating model name prefix.
+for key in model_to_be_adapt:
+    if key.startswith(pretrained_weights_prefix):
+        new_key = key[len(pretrained_weights_prefix):]
+        new_state_dict[new_key] = model_to_be_adapt[key]
+
+# Step 2: Truncating weight size of embeddings using real target vocab size.
+new_state_dict[vocab_embedding_key] = new_state_dict[vocab_embedding_key][:real_vocab_size]
+
+# Step 3: Load extra weights from reference model that are not appearing in the loaded model.
+for key in extra_weights_need_to_be_add:
+    new_state_dict[key] = extra_reference_model[key]
+
+torch.save(new_state_dict, target_dump_path)
+
+print(f'Done, save to {target_dump_path}')
+
