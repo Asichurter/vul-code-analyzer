f59828ce148487bc815567ba45d8ad3e5c8efae1
Asichurter <asichurter@gmail.com>
2022-10-28 14:12:39 +0800

    adapt unixcoder for check

--- utils/pretrain_utils/check.py
+++ utils/pretrain_utils/check.py
@@ -3,6 +3,8 @@ from typing import List, Optional
 import torch
 from allennlp.data import TextFieldTensors, Vocabulary, Token
 
+from utils.allennlp_utils.tokenizer_vocab_sensitive_utils import codebert_families, unixcoder_families, microsoft_pretrain_families
+
 
 def check_identifier_matching(edges: torch.Tensor,
                               code: TextFieldTensors,
@@ -24,10 +26,15 @@ def check_pretrain_code_field_correctness(special_tokenizer_token_handler_type: 
                                           original_code: str,
                                           tokenized_code: List[Token],
                                           line_indexes: torch.Tensor,
-                                          edge_matrix: Optional[torch.Tensor] = None):
+                                          edge_matrix: Optional[torch.Tensor] = None,
+                                          input_mode_count: int = 1):
     # 1. Check tokenized code
-    if special_tokenizer_token_handler_type == 'codebert':
-        if len(tokenized_code) == 2:
+    if special_tokenizer_token_handler_type in codebert_families:
+        if len(tokenized_code) <= 2:
+            mylogger.error('check_pretrain_code_field_correctness',
+                           f'Found empty tokenized code, original code: {original_code}')
+    elif special_tokenizer_token_handler_type in unixcoder_families:
+        if len(tokenized_code) <= 4:
             mylogger.error('check_pretrain_code_field_correctness',
                            f'Found empty tokenized code, original code: {original_code}')
     else:
@@ -35,9 +42,14 @@ def check_pretrain_code_field_correctness(special_tokenizer_token_handler_type: 
                          f'Unhandled tokenized type: {special_tokenizer_token_handler_type}')
 
     # 2. Check consistency between code and line index
-    if special_tokenizer_token_handler_type == 'codebert':
-        if len(tokenized_code) - 2 != len(line_indexes):
-            mylogger.error('check_pretrain_code_field_correctness',
-                           f'Inconsistency found between line index and tokenized code: ' +
-                           f'code_len({len(tokenized_code)}) - 2 != index_len({len(line_indexes)}). '
-                           f'\noriginal code: {original_code}')
+    if special_tokenizer_token_handler_type in codebert_families:
+        special_token_count = 1 + input_mode_count
+    elif special_tokenizer_token_handler_type in unixcoder_families:
+        special_token_count = 3 + input_mode_count
+    else:
+        raise NotImplementedError
+    if len(tokenized_code) - special_token_count != len(line_indexes):
+        mylogger.error('check_pretrain_code_field_correctness',
+                       f'Inconsistency found between line index and tokenized code: ' +
+                       f'code_len({len(tokenized_code)}) - {special_token_count} != index_len({len(line_indexes)}). '
+                       f'\noriginal code: {original_code}')
