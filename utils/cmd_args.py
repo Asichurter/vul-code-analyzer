1276359b3b4d41684555c19e61d07e2334dd595d
Asichurter <asichurter@gmail.com>
2022-11-11 15:15:20 +0800

    add eval args and cmd arg util

--- utils/cmd_args.py
+++ utils/cmd_args.py
@@ -1,8 +1,27 @@
 import argparse
+from typing import Dict
+
+
+def make_cli_args(one_bar_args: Dict, two_bar_args: Dict):
+    args = ''
+    for argkey, argval in one_bar_args.items():
+        if argval is None:
+            arg = f' -{argkey}'
+        else:
+            arg = f' -{argkey} {argval}'
+        args += arg
+    for argkey, argval in two_bar_args.items():
+        if argval is None:
+            arg = f' --{argkey}'
+        else:
+            arg = f' --{argkey} {argval}'
+        args += arg
+    return args
 
 def read_train_from_config_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-config', type=str, default='', help='config path of training')
+    parser.add_argument('-run_log_dir', type=str, default='reveal_new', help='config path of training')
     parser.add_argument('--disable_friendly_logging', action='store_true', help='whether to disable file_friendly_logging of allennlp.train')
     return parser.parse_args()
 
@@ -16,6 +35,7 @@ def read_reveal_cv_train_from_config_args():
     parser.add_argument('-test_filenames', type=str, default='model.tar.gz', help='model file names after training, splitted by comma')
     parser.add_argument('-eval_script', type=str, default='evaluate_reveal.py', help='model file names after training, splitted by comma')
     parser.add_argument('-title', type=str, default='', help='title for reporting cv results')
+    parser.add_argument('-test_file_name', type=str, default='test.json', help='filename of the tested data')
     parser.add_argument('--no_train', action='store_true', help='whether do train from scratch')
     parser.add_argument('--disable_friendly_logging', action='store_true', help='whether to disable file_friendly_logging of allennlp.train')
     return parser.parse_args()
@@ -30,6 +50,31 @@ def read_reveal_eval_args():
     parser.add_argument('-run_log_dir', default='reveal_new', type=str)
     parser.add_argument('-split', required=True, type=str)
     parser.add_argument('-cuda', type=int)
+    return parser.parse_args()
+
+def read_fan_vul_det_eval_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-version', required=True, type=int)
+    parser.add_argument('-subfolder', required=True, type=str, help="folder of which format, such as `splits`")
+    parser.add_argument('-subset', required=True, type=str, help='which split to use')
+    parser.add_argument('-model_name', type=str, default='model.tar.gz')
+    parser.add_argument('-data_file_name', type=str, default='test.pkl')
+    parser.add_argument('-run_log_dir', default='fan_vul_det', type=str)
+    parser.add_argument('-cuda', type=int)
+    parser.add_argument('-batch_size', default=64, type=int)
+    return parser.parse_args()
+
+def read_devign_eval_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-version', required=True, type=int)
+    parser.add_argument('-subfolder', required=True, type=str, help="folder of which format, such as `splits`")
+    parser.add_argument('-subset', required=True, type=str, help='which split to use')
+    parser.add_argument('-split', required=True, type=str)
+    parser.add_argument('-model_name', type=str, default='model.tar.gz')
+    parser.add_argument('-data_file_name', type=str, default='test.pkl')
+    parser.add_argument('-run_log_dir', default='devign', type=str)
+    parser.add_argument('-cuda', type=int)
+    parser.add_argument('-batch_size', default=64, type=int)
     return parser.parse_args()
 
 def read_aggre_eval_results_args():
