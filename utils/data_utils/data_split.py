4cae1c40b517a1abcec0a0627f3e201ecf7a1ea3
Asichurter <asichurter@gmail.com>
2022-11-11 15:13:24 +0800

    add util for class-sensitive random splits

--- utils/data_utils/data_split.py
+++ utils/data_utils/data_split.py
@@ -1,5 +1,7 @@
 import random
 import time
+from typing import Iterable, Dict
+
 
 def split_cross_validation_data(data_items, cv):
     cv_num = len(data_items) // cv + 1
@@ -32,3 +34,20 @@ def random_split(data_list, train_ratio, validate_ratio):
     return data_list[:train_num], \
            data_list[train_num:train_num+validate_num], \
            data_list[-test_num:]
+
+def class_sensitive_random_split(data_list: Iterable[Dict], train_ratio, validate_ratio, label_key: str, shuffle=True):
+    trains, validates, tests = [], [], []
+    labels = set([d[label_key] for d in data_list])
+    for label in labels:
+        labeled_data = [d for d in data_list if d[label_key]==label]
+        labeled_train, labeled_val, labeled_test = random_split(labeled_data, train_ratio, validate_ratio)
+        trains += labeled_train
+        validates += labeled_val
+        tests += labeled_test
+
+    if shuffle:
+        random.shuffle(trains)
+        random.shuffle(validates)
+        random.shuffle(tests)
+
+    return trains, validates, tests
