1e52bed03dabc424c0f744deaad6b1c7e9766e33
Asichurter <asichurter@gmail.com>
2022-11-29 21:10:06 +0800

    add light-weight pooler

--- common/__init__.py
+++ common/__init__.py
@@ -1,6 +1,7 @@
 from .modules.callbacks import EpochPrintCallback, ModelParamStatCallback, SaveEpochModelCallback, SaveEpochStateCallback, PartialLoadStateDictCallback
 from .nn.classifier import LinearSoftmaxClassifier, LinearSigmoidClassifier
 from .nn.loss_func import NllLoss, BinaryCrossEntropyLoss, CrossEntropyLoss, BCELoss, BCEFocalLoss
+from .nn.pooler import ClsPooler, MeanPooler
 from .modules.code_cleaner import SpaceSubCodeCleaner, PreLineTruncateCodeCleaner
 from .modules.balanced_samplers import BinaryDuplicateBalancedIterSampler, BinaryDuplicateBalancedRandSampler
 from .modules.tokenizer.pretrained_mismatch_truncate_tokenizer import PretrainedMismatchTruncateTokenizer
