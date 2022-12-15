3d2a34bd7f750c581ff9bbca5d2cd15bf969737b
Asichurter <asichurter@gmail.com>
2022-12-16 00:47:42 +0800

    add list-type class-wise data split

--- utils/data_utils/data_split.py
+++ utils/data_utils/data_split.py
@@ -1,6 +1,6 @@
 import random
 import time
-from typing import Iterable, Dict
+from typing import Iterable, Dict, List
 
 
 def split_cross_validation_data(data_items, cv):
@@ -40,6 +40,23 @@ def class_sensitive_random_split(data_list: Iterable[Dict], train_ratio, validat
     labels = set([d[label_key] for d in data_list])
     for label in labels:
         labeled_data = [d for d in data_list if d[label_key]==label]
+        labeled_train, labeled_val, labeled_test = random_split(labeled_data, train_ratio, validate_ratio)
+        trains += labeled_train
+        validates += labeled_val
+        tests += labeled_test
+
+    if shuffle:
+        random.shuffle(trains)
+        random.shuffle(validates)
+        random.shuffle(tests)
+
+    return trains, validates, tests
+
+def class_sensitive_random_split_by_list(data_list: Iterable[List], train_ratio, validate_ratio, label_index: int, shuffle=True):
+    trains, validates, tests = [], [], []
+    labels = set([d[label_index] for d in data_list])
+    for label in labels:
+        labeled_data = [d for d in data_list if d[label_index]==label]
         labeled_train, labeled_val, labeled_test = random_split(labeled_data, train_ratio, validate_ratio)
         trains += labeled_train
         validates += labeled_val
