1179c9ceab2711fad01dd17a7c62fc4a01856503
Asichurter <asichurter@gmail.com>
2023-01-29 13:48:58 +0800

    update predict_pdg to monitor time

--- pretrain/predict_pdg.py
+++ pretrain/predict_pdg.py
@@ -1,3 +1,4 @@
+import time
 import torch
 
 from allennlp.common import JsonDict
@@ -6,10 +7,20 @@ from allennlp.predictors import Predictor
 from allennlp.models import Model
 from allennlp.data.tokenizers import PretrainedTransformerTokenizer
 
+from transformers import (WEIGHTS_NAME, AdamW, get_linear_schedule_with_warmup,
+                          BertConfig, BertForMaskedLM, BertTokenizer,
+                          GPT2Config, GPT2LMHeadModel, GPT2Tokenizer,
+                          OpenAIGPTConfig, OpenAIGPTLMHeadModel, OpenAIGPTTokenizer,
+                          RobertaConfig, RobertaForSequenceClassification, RobertaTokenizer,
+                          DistilBertConfig, DistilBertForMaskedLM, DistilBertTokenizer)
+
+from transformers.models.roberta.modeling_roberta import RobertaClassificationHead
+
 from pretrain import *
 from common import *
-from utils.file import load_text
+from utils.file import load_text, read_dumped, dump_text
 from utils.allennlp_utils.build_utils import build_dataset_reader_from_config
+from utils.joern_utils.pretty_print_utils import multi_paired_token_colored_print, print_code_with_line_num
 
 class PDGPredictor(Predictor):
     def predict_pdg(self, code: str):
@@ -30,7 +41,7 @@ def set_reader(_reader):
     _reader.is_train = False
     return _reader
 
-cuda_device = 4
+cuda_device = 7
 model_path = '/data1/zhijietang/vul_data/run_logs/pretrain/' + '57/' + 'model_epoch_9.tar.gz'
 config_path = '/data1/zhijietang/vul_data/run_logs/pretrain/' + '57/' + 'config.json'
 code_path = '/data1/zhijietang/temp/test.c'
@@ -47,9 +58,53 @@ dataset_reader = build_dataset_reader_from_config(config_path)
 dataset_reader = set_reader(dataset_reader)
 predictor = PDGPredictor(model, dataset_reader, frozen=True)
 
-print(f'[main] Predicting {code_path}')
-code = load_text(code_path)
-pdg_output = predictor.predict_pdg(code)
+##################  For Pred Time Statistics ##################
 
-cdg = torch.Tensor(pdg_output['ctrl_edge_labels'])
-ddg = torch.Tensor(pdg_output['data_edge_labels'])
+code_pkl = read_dumped('/data1/zhijietang/vul_data/datasets/joern_vulberta/packed_process_hybrid_data/packed_hybrid_vol_0.pkl')
+
+pdg_pred_secs = []
+for i in range(100):
+    code = code_pkl[i]['raw_code']
+    dump_text(code, code_path)
+
+    start = time.time()
+    code = load_text(code_path)
+    tokens = tokenizer.tokenize(code)
+    pdg_output = predictor.predict_pdg(code)
+    end = time.time()
+    pdg_pred_secs.append(end-start)
+
+    cdg = torch.Tensor(pdg_output['ctrl_edge_labels'])
+    ddg = torch.Tensor(pdg_output['data_edge_labels'])
+    ddg_list = ddg.nonzero().tolist()
+
+    if len(ddg_list) > 0:
+        print('\n')
+        multi_paired_token_colored_print(code, ddg_list, tokens, processed=True)
+
+    print('\n')
+    print_code_with_line_num(code, start_line_num=0)
+    print(f'\n{cdg.nonzero().tolist()}')
+
+    print('\n')
+    print('#'*70 + '\n')
+
+print(f'Avg PDG predict time: {sum(pdg_pred_secs) / len(pdg_pred_secs)}')
+
+
+##################  For Single Code File ##################
+
+# print(f'[main] Predicting {code_path}')
+# code = load_text(code_path)
+# tokens = tokenizer.tokenize(code)
+# pdg_output = predictor.predict_pdg(code)
+#
+# cdg = torch.Tensor(pdg_output['ctrl_edge_labels'])
+# ddg = torch.Tensor(pdg_output['data_edge_labels'])
+#
+# print('\n\n')
+# multi_paired_token_colored_print(code, ddg.nonzero().tolist(), tokens, processed=True)
+#
+# print('\n\n')
+# print_code_with_line_num(code, start_line_num=0)
+# print(f'\n{cdg.nonzero().tolist()}')
