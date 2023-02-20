9f5246f01fbe4b14bcec607c5a5a116b6efc200f
Asichurter <asichurter@gmail.com>
2023-02-20 14:13:33 +0800

    update predict_pdg

--- pretrain/predict_pdg.py
+++ pretrain/predict_pdg.py
@@ -1,5 +1,7 @@
 import time
 import torch
+import os
+import sys
 
 from allennlp.common import JsonDict
 from allennlp.data import Instance
@@ -20,7 +22,7 @@ from pretrain import *
 from common import *
 from utils.file import load_text, read_dumped, dump_text
 from utils.allennlp_utils.build_utils import build_dataset_reader_from_config
-from utils.joern_utils.pretty_print_utils import multi_paired_token_colored_print, print_code_with_line_num
+from utils.joern_utils.pretty_print_utils import multi_paired_token_colored_print, print_code_with_line_num, multi_paired_token_tagged_print
 
 class PDGPredictor(Predictor):
     def predict_pdg(self, code: str):
@@ -44,8 +46,11 @@ def set_reader(_reader):
 cuda_device = 7
 model_path = '/data1/zhijietang/vul_data/run_logs/pretrain/' + '57/' + 'model_epoch_9.tar.gz'
 config_path = '/data1/zhijietang/vul_data/run_logs/pretrain/' + '57/' + 'config.json'
-code_path = '/data1/zhijietang/temp/test.c'
+code_path = '/data1/zhijietang/temp/joern_failed_case_1.cpp'
 tokenizer_name = 'microsoft/codebert-base'
+
+# f_output = open("/data1/zhijietang/temp/joern_failed_cases/joern_failed_cases_summary", "w")
+# sys.stdout = f_output
 
 torch.cuda.set_device(cuda_device)
 print(f'[main] Building tokenizer: {tokenizer_name}')
@@ -60,51 +65,70 @@ predictor = PDGPredictor(model, dataset_reader, frozen=True)
 
 ##################  For Pred Time Statistics ##################
 
-code_pkl = read_dumped('/data1/zhijietang/vul_data/datasets/joern_vulberta/packed_process_hybrid_data/packed_hybrid_vol_0.pkl')
-
-pdg_pred_secs = []
-for i in range(100):
-    code = code_pkl[i]['raw_code']
-    dump_text(code, code_path)
-
-    start = time.time()
-    code = load_text(code_path)
-    tokens = tokenizer.tokenize(code)
-    pdg_output = predictor.predict_pdg(code)
-    end = time.time()
-    pdg_pred_secs.append(end-start)
-
-    cdg = torch.Tensor(pdg_output['ctrl_edge_labels'])
-    ddg = torch.Tensor(pdg_output['data_edge_labels'])
-    ddg_list = ddg.nonzero().tolist()
-
-    if len(ddg_list) > 0:
-        print('\n')
-        multi_paired_token_colored_print(code, ddg_list, tokens, processed=True)
-
-    print('\n')
-    print_code_with_line_num(code, start_line_num=0)
-    print(f'\n{cdg.nonzero().tolist()}')
-
-    print('\n')
-    print('#'*70 + '\n')
-
-print(f'Avg PDG predict time: {sum(pdg_pred_secs) / len(pdg_pred_secs)}')
+# code_pkl = read_dumped('/data1/zhijietang/vul_data/datasets/joern_vulberta/packed_process_hybrid_data/packed_hybrid_vol_0.pkl')
+#
+# pdg_pred_secs = []
+# for i in range(100):
+#     code = code_pkl[i]['raw_code']
+#     dump_text(code, code_path)
+#
+#     start = time.time()
+#     code = load_text(code_path)
+#     tokens = tokenizer.tokenize(code)
+#     pdg_output = predictor.predict_pdg(code)
+#     end = time.time()
+#     pdg_pred_secs.append(end-start)
+#
+#     cdg = torch.Tensor(pdg_output['ctrl_edge_labels'])
+#     ddg = torch.Tensor(pdg_output['data_edge_labels'])
+#     ddg_list = ddg.nonzero().tolist()
+#
+#     if len(ddg_list) > 0:
+#         print('\n')
+#         multi_paired_token_colored_print(code, ddg_list, tokens, processed=True)
+#
+#     print('\n')
+#     print_code_with_line_num(code, start_line_num=0)
+#     print(f'\n{cdg.nonzero().tolist()}')
+#
+#     print('\n')
+#     print('#'*70 + '\n')
+#
+# print(f'Avg PDG predict time: {sum(pdg_pred_secs) / len(pdg_pred_secs)}')
 
 
 ##################  For Single Code File ##################
 
-# print(f'[main] Predicting {code_path}')
-# code = load_text(code_path)
-# tokens = tokenizer.tokenize(code)
-# pdg_output = predictor.predict_pdg(code)
+def predict_one_file(code_file_path):
+    print(f'[main] Predicting {code_file_path}')
+    code = load_text(code_file_path)
+    tokens = tokenizer.tokenize(code)
+    pdg_output = predictor.predict_pdg(code)
+
+    cdg = torch.Tensor(pdg_output['ctrl_edge_labels'])
+    ddg = torch.Tensor(pdg_output['data_edge_labels'])
+
+    print('\n\n' + 'Data-Depedency:\n')
+    # multi_paired_token_colored_print(code, ddg.nonzero().tolist(), tokens, processed=True)
+    multi_paired_token_tagged_print(code, ddg.nonzero().tolist(), tokens, processed=True)
+
+    print('\n\n')
+    print_code_with_line_num(code, start_line_num=0)
+    print(f'\nCtrl-Dependency: {cdg.nonzero().tolist()}')
+
+# predict_one_file(code_path)
+
+##################  For Dumping Results of Batched Files ##################
+
+# files_dir_path = '/data1/zhijietang/temp/joern_failed_cases'
+# results_dump_path = ''
+# results = []
 #
-# cdg = torch.Tensor(pdg_output['ctrl_edge_labels'])
-# ddg = torch.Tensor(pdg_output['data_edge_labels'])
+# for file in os.listdir(files_dir_path):
+#     if file.split('.')[-1] != 'cpp':
+#         continue
+#     file_path = os.path.join(files_dir_path, file)
+#     print('\n\n\n' + '-'*75)
+#     predict_one_file(file_path)
 #
-# print('\n\n')
-# multi_paired_token_colored_print(code, ddg.nonzero().tolist(), tokens, processed=True)
-#
-# print('\n\n')
-# print_code_with_line_num(code, start_line_num=0)
-# print(f'\n{cdg.nonzero().tolist()}')
+# f_output.close()
