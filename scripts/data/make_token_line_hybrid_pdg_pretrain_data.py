7c68f714467a693fe144538d2f5f8c291dcce7c4
Asichurter <asichurter@gmail.com>
2023-03-16 11:15:40 +0800

    support "update_field" of data packaging

--- scripts/data/make_token_line_hybrid_pdg_pretrain_data.py
+++ scripts/data/make_token_line_hybrid_pdg_pretrain_data.py
@@ -5,7 +5,8 @@ from tqdm import tqdm
 from allennlp.data.tokenizers import PretrainedTransformerTokenizer
 
 from utils.file import load_pickle, dump_pickle, load_text
-from utils.joern_utils.joern_dev_pdg_parse_utils import build_token_level_pdg_struct, build_line_level_pdg_struct
+from utils.joern_utils.joern_dev_pdg_parse_utils import build_token_level_pdg_struct, build_line_level_pdg_struct, \
+    build_line_level_cfg_struct
 from utils.joern_utils.joern_dev_parse import convert_func_signature_to_one_line
 from utils.allennlp_utils.tokenizer_vocab_sensitive_utils import pre_handle_special_tokenizer_tokens, post_handle_special_tokenizer_tokens
 
@@ -171,6 +172,77 @@ def update_token_data_tokenizer(check_overwrite=True):
         dump_pickle(tgt_vol_data, tgt_dump_base_path_temp.format(vol))
 
 
+def update_field(check_overwrite=True):
+    raw_code_base_vol_path = '/data1/zhijietang/vul_data/datasets/docker/cppfiles/'
+    line_level_data_path_temp = '/data1/zhijietang/vul_data/datasets/joern_vulberta/packed_vol_data/packed_vol_{}.pkl'
+    token_level_data_path_temp = '/data1/zhijietang/vul_data/datasets/docker/joern_dev_analysis_results/joern_parsed_raw_vol{}.pkl'
+    tgt_dump_base_path_temp = '/data1/zhijietang/vul_data/datasets/joern_vulberta/packed_process_hybrid_data/packed_hybrid_vol_{}.pkl'
+    # tgt_dump_base_path_temp = '/data1/zhijietang/vul_data/datasets/joern_vulberta/packed_process_hybrid_data_allvs/packed_hybrid_vol_{}.pkl'
+
+    multi_vs_multi_strategy = 'first'
+
+    tokenizer_name = 'microsoft/codebert-base'
+    tokenizer_name_postfix = ''
+    tokenizer_type = 'codebert'
+    # tokenizer = PretrainedTransformerTokenizer(tokenizer_name)
+    new_field_key = 'cfg_edges'
+
+    vols = list(range(180,229))
+
+    for vol in vols:
+        line_vol_data_path = line_level_data_path_temp.format(vol)
+        token_vol_data_path = token_level_data_path_temp.format(vol)
+        tgt_vol_data_path = tgt_dump_base_path_temp.format(vol)
+        line_vol_data = load_pickle(line_vol_data_path)
+        token_vol_data = load_pickle(token_vol_data_path)
+        tgt_vol_data = load_pickle(tgt_vol_data_path)
+
+        line_vol_data_id_map = {o['id']: o for o in line_vol_data}
+        id_to_new_field_datas_idx_map = {}
+        new_field_datas = []
+
+        for token_data in tqdm(token_vol_data):
+            data_id = extract_data_id(token_data['file_path'])
+            if data_id not in line_vol_data_id_map:
+                print(f'[Warn] Missing line-level item for vol {vol}, id {data_id}')
+                continue
+
+            # line_data = line_vol_data_id_map[data_id]
+            # raw_code = convert_func_signature_to_one_line(code=line_data['code'], redump=False)
+            # tokens = tokenizer.tokenize(raw_code)
+            # # BugFix: Since we will do some special token preprocesses on input tokens,
+            # #         we need to simulate these during preprocessing to accurately analyze the token intersections.
+            # # ------------------------------------------------------------------------------------------------
+            # tokens = pre_handle_special_tokenizer_tokens(tokenizer_type, tokens)
+            # tokens, _ = post_handle_special_tokenizer_tokens(tokenizer_type, (tokens,), None,
+            #                                                  '<encoder-only>')  # mode is only for placeholder
+            # # ------------------------------------------------------------------------------------------------
+            # _, token_data_edges = build_token_level_pdg_struct(raw_code, tokens,
+            #                                                    token_data['nodes'], token_data['edges'],
+            #                                                    multi_vs_multi_strategy=multi_vs_multi_strategy,
+            #                                                    to_build_token_ctrl_edges=False)
+            # pdg_data_edges = parse_dependency_strs(token_data_edges)
+            # id_to_new_field_datas_idx_map[data_id] = len(new_field_datas)
+            # new_field_datas.append(pdg_data_edges)
+
+            line_cfg_edges = build_line_level_cfg_struct(token_data['nodes'], token_data['edges'])
+            parsed_cfg_line_edges = parse_dependency_strs(line_cfg_edges)
+            id_to_new_field_datas_idx_map[data_id] = len(new_field_datas)
+            new_field_datas.append(parsed_cfg_line_edges)
+
+        for tgt_data in tgt_vol_data:
+            data_id = tgt_data['id']
+            new_field_data = new_field_datas[id_to_new_field_datas_idx_map[data_id]]
+
+            if check_overwrite and new_field_key in tgt_data:
+                assert False, f'check_overwrite=True and {new_field_key} has existed in data obj!'
+            tgt_data[new_field_key] = new_field_data
+
+        print(f'Vol. {vol} ({len(tgt_vol_data)} items) saved to {tgt_dump_base_path_temp.format(vol)}')
+        dump_pickle(tgt_vol_data, tgt_dump_base_path_temp.format(vol))
+
+
 if __name__ == '__main__':
-    generate_data_from_scratch()
+    # generate_data_from_scratch()
     # update_token_data_tokenizer(False)
+    update_field(True)
