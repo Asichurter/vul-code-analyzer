8b7407fd2d5bcf72c698449e0399c45ef3c072a5
Asichurter <asichurter@gmail.com>
2023-07-28 02:35:04 +0800

    add full-func version PDG pred

---
+++ pretrain/eval/eval_full_func_pdg_on_my_data.py
@@ -0,0 +1,295 @@
+import re
+import time
+from typing import List, Dict
+import torch
+from sklearn.metrics import f1_score
+from tqdm import tqdm
+import numpy
+import sys
+
+sys.path.append("/data2/zhijietang/projects/vul-code-analyzer")
+
+cuda_device = 1
+
+from allennlp.common import JsonDict
+from allennlp.data import Instance, Token
+from allennlp.predictors import Predictor
+from allennlp.models import Model
+from allennlp.data.tokenizers import PretrainedTransformerTokenizer
+
+from common.modules.code_cleaner import TrivialCodeCleaner
+from pretrain import *
+from common import *
+from utils.file import read_dumped, dump_json
+from utils.allennlp_utils.build_utils import build_dataset_reader_from_config
+from utils.joern_utils.pretty_print_utils import print_code_with_line_num
+from utils.joern_utils.joern_dev_parse import convert_func_signature_to_one_line
+
+class PDGPredictor(Predictor):
+    def predict_pdg(self, code: str):
+        instance = self._json_to_instance({
+            'raw_code': code
+        })
+        return self.predict_instance(instance)
+
+    def _json_to_instance(self, json_dict: JsonDict) -> Instance:
+        ok, instance = self._dataset_reader.text_to_instance(json_dict)
+        if not ok:
+            raise ValueError
+        else:
+            return instance
+
+
+def set_reader(_reader: CodeAnalyLinePretrainReader, _max_lines):
+    _reader.is_train = False
+    _reader.code_cleaner = TrivialCodeCleaner()     # To avoid multiple nl elimination, may affect performance
+    # _reader.pdg_max_vertice = _max_lines
+    # _reader.max_lines = _max_lines
+    return _reader
+
+def build_my_partial_ground_truth_pdg(data_item: Dict,
+                                      raw_code: str,
+                                      tokens: List[Token],
+                                      data_dep_label_key: str,
+                                      total_line: int,
+                                      Ns: List[int]):
+    total_token = len(tokens)
+    full_total_line = raw_code.count('\n') + 2
+
+    pdg_ctrl_graph = torch.zeros((full_total_line, full_total_line))
+    pdg_data_graph = torch.zeros((10000, 10000))        # No need of placeholder for tokens
+    if len(data_item['line_edges']) != 0:
+        pdg_ctrl_edge_list = torch.LongTensor(data_item['line_edges'])
+        pdg_ctrl_graph[pdg_ctrl_edge_list[:,0],pdg_ctrl_edge_list[:,1]] = 1
+    # Drop first line
+    pdg_ctrl_graph = pdg_ctrl_graph[1:total_line+1, 1:total_line+1]
+
+    pdg_data_edge_list = data_item['processed_token_data_edges'][data_dep_label_key]
+    if len(pdg_data_edge_list) != 0:
+        pdg_data_edge_list = torch.LongTensor(pdg_data_edge_list)
+        pdg_data_graph[pdg_data_edge_list[:,0],pdg_data_edge_list[:,1]] = 1
+    pdg_data_graph = pdg_data_graph[:total_token, :total_token]
+
+    # Find the char indices of new-lines
+    new_line_indices = []
+    for m in re.finditer('\n', raw_code):
+        new_line_indices.append(m.start())
+    # Add a dummy nl at last to avoid out-of-bound
+    new_line_indices.append(1e10)
+
+    line_limit_to_token_idx = {}
+    cur_line = 1
+    cur_nl_idx = 0
+    for i, t in enumerate(tokens):
+        if t.idx is None:
+            continue
+        while t.idx <= new_line_indices[cur_nl_idx] < t.idx_end:
+            line_limit_to_token_idx[cur_line] = i
+            cur_line += 1
+            cur_nl_idx += 1
+    # Compensate for missing last nl
+    line_limit_to_token_idx[total_line] = len(tokens) - 1
+
+    for n in Ns:
+        if n > total_line:
+            yield [], []
+        else:
+            token_n = line_limit_to_token_idx[n]
+            yield pdg_ctrl_graph[:n, :n].flatten().tolist(), \
+                  pdg_data_graph[:token_n+1, :token_n+1].flatten().tolist()
+
+def get_line_count_from_tokens(raw_code: str, tokens: List[Token]):
+    """
+        Compute the line count from tokens, where raw_code is complete but tokens are partial.
+    """
+    # Find the char indices of new-lines
+    new_line_indices = []
+    for m in re.finditer('\n', raw_code):
+        new_line_indices.append(m.start())
+    # Add a dummy nl at last to avoid out-of-bound
+    new_line_indices.append(1e10)
+
+    cur_line = 0
+    cur_nl_idx = 0
+    for i, t in enumerate(tokens):
+        if t.idx is None:
+            continue
+        while t.idx <= new_line_indices[cur_nl_idx] <= t.idx_end:
+            cur_line += 1
+            cur_nl_idx += 1
+    return cur_line
+
+def split_partial_code(code: str, n_line: int):
+    code_lines = code.split("\n")
+    return '\n'.join(code_lines[:n_line])
+
+def predict_one_file(code, ctrl_edges, data_edges, n):
+    code_snippet = split_partial_code(code, n)
+    pdg_output = predictor.predict_pdg(code_snippet)
+    ctrl_pred, data_pred = pdg_output['ctrl_edge_labels'], pdg_output['data_edge_labels']
+    ctrl_pred = torch.IntTensor(ctrl_pred).flatten().tolist()
+    data_pred = torch.IntTensor(data_pred).flatten().tolist()
+
+    ctrl_label, data_label, line_count = dataset_reader.process_test_labels(code_snippet, ctrl_edges, data_edges)
+    # Minus one to revert the real matrix.
+    ctrl_label = (ctrl_label-1).flatten().tolist()
+    data_label = (data_label-1).flatten().tolist()
+
+    if line_count <= n:
+        return ctrl_pred, data_pred, ctrl_label, data_label
+    else:
+        return [], [], [], []
+
+def process_my_code(code: str):
+    """
+        Since in the original tokenization system, multiple new lines were not properly
+        handled causing line count error.
+        Here we insert a space into them to prevent from this special case.
+    """
+    if code[-1] != '\n':
+        return code + '\n'
+    else:
+        return code
+
+def process_result_as_conf_matrix(predicts, labels):
+    """
+    Return: TN, FN, FP, TP
+    """
+    p_tensor = numpy.array(predicts, dtype=int)
+    l_tensor = numpy.array(labels, dtype=int)
+    indices = numpy.arange(len(predicts))
+    m = numpy.zeros((len(predicts), 2, 2), dtype=int)
+    m[indices, p_tensor, l_tensor] = 1
+    return m.sum(0)
+
+def cal_f1_from_conf_matrix(conf_m):
+    TN, FN, FP, TP = conf_m.flatten()
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    return 2*precision*recall / (precision + recall)
+
+#---------------------------------------------------------------------------
+
+PATH_PREFIX = 'data2'
+
+model_path = f'/{PATH_PREFIX}/zhijietang/vul_data/run_logs/pretrain/' + '57/' + 'model.tar.gz'
+config_path = f'/{PATH_PREFIX}/zhijietang/vul_data/run_logs/pretrain/' + '57/' + 'config.json'
+tokenizer_name = 'microsoft/codebert-base'
+
+max_lines = 50
+# Last N is only for archoring, not valid
+full_Ns = [3, 5, 8, 10, 12, 15, 18, 20, 22, 25, 27, 30, 10000]
+
+# f_output = open("/data1/zhijietang/temp/joern_failed_cases/joern_failed_cases_summary", "w")
+# sys.stdout = f_output
+
+#---------------------------------------------------------------------------
+
+torch.cuda.set_device(cuda_device)
+print('\n\n')
+# print(f'[main] Building tokenizer: {tokenizer_name}\n')
+# tokenizer = PretrainedTransformerTokenizer(tokenizer_name)
+print(f'[main] Building model from: {model_path}\n')
+model = Model.from_archive(model_path)
+model = model.cuda(cuda_device)
+print(f'[main] Building reader from: {config_path}\n')
+dataset_reader = build_dataset_reader_from_config(config_path)
+dataset_reader = set_reader(dataset_reader, max_lines)
+predictor = PDGPredictor(model, dataset_reader, frozen=True)
+
+#---------------------------------------------------------------------------
+
+full_ctrl_results = {k:numpy.zeros((2, 2), dtype=int) for k in full_Ns}
+full_data_results = {k:numpy.zeros((2, 2), dtype=int) for k in full_Ns}
+full_counts = {n:0 for n in full_Ns}
+
+#---------------------------------------------------------------------------
+
+def eval_full_func_for_my_data(data_base_path,
+                               file_name_temp,
+                               dump_path,
+                               vol_range,
+                               max_tokens=512):
+    print("Building components...")
+    svol, evol = vol_range
+    vols = list(range(svol, evol+1))
+    pretrained_model = 'microsoft/codebert-base'
+
+    # No inner truncation to identify full function
+    tokenizer = PretrainedTransformerTokenizer(pretrained_model)
+    results = {}
+    total_instance = 0
+
+    for vol in vols:
+        test_file_path = data_base_path + file_name_temp.format(vol)
+        print(f'Eval on Vol.{vol} ...')
+        data_items = read_dumped(test_file_path)
+        total_instance += len(data_items)
+        for i, data_item in tqdm(enumerate(data_items), total=len(data_items)):
+            raw_code = data_item['raw_code']
+            raw_code = process_my_code(convert_func_signature_to_one_line(code=raw_code, redump=False))
+            tokens = tokenizer.tokenize(raw_code)
+            # Not full within max_tokens, skip
+            if len(tokens) > max_tokens:
+                continue
+            max_lines = get_line_count_from_tokens(raw_code, tokens)
+            # labels_generator = build_my_partial_ground_truth_pdg(data_item, raw_code, tokens, pretrained_model, max_lines, Ns)
+            for n in full_Ns:
+                # Only work on the first n larger than max_lines, to locale range
+                if n >= max_lines:
+                    try:
+                        # Set n=max_lines to reuse utils
+                        cdg_preds, ddg_preds, cdg_labels, ddg_labels = predict_one_file(raw_code,
+                                                                                        data_item['line_edges'],
+                                                                                        data_item['processed_token_data_edges'][pretrained_model],
+                                                                                        max_lines)
+                    except Exception as e:
+                        print(f"Error when predicting #{i}, n={n}, err_type: {type(e)}, err: {e}, skipped")
+                        continue
+
+                    assert len(cdg_preds) == len(cdg_labels), \
+                           f"CDG: pred ({len(cdg_preds)}) != label ({len(cdg_labels)}). \n- Code: {raw_code}"
+                    assert len(ddg_preds) == len(ddg_labels), \
+                           f"DDG: pred ({len(ddg_preds)}) != label ({len(ddg_labels)}). \n- Code: {raw_code}"
+
+                    ctrl_res_m = process_result_as_conf_matrix(cdg_preds, cdg_labels)
+                    data_res_m = process_result_as_conf_matrix(ddg_preds, ddg_labels)
+                    full_ctrl_results[n] += ctrl_res_m
+                    full_data_results[n] += data_res_m
+                    full_counts[n] += 1
+                    break
+
+    for n in full_Ns:
+        c_pairs = int(full_ctrl_results[n].sum())
+        d_pairs = int(full_data_results[n].sum())
+        c_f1 = cal_f1_from_conf_matrix(full_ctrl_results[n]) if c_pairs > 0 else None
+        d_f1 = cal_f1_from_conf_matrix(full_data_results[n]) if d_pairs > 0 else None
+        overall_f1 = cal_f1_from_conf_matrix(full_ctrl_results[n] + full_data_results[n]) if c_pairs + d_pairs > 0 else None
+        n_result = {
+            'total_instance': total_instance,
+            'total_valid_instance': full_counts[n],
+            'c_pairs': c_pairs,
+            'd_pairs': d_pairs,
+            'ctrl_f1': c_f1 ,
+            'data_f1': d_f1,
+            'overall_f1': overall_f1
+        }
+        results[n] = n_result
+        print("\n" + '*' * 50)
+        print(f"N = {n}")
+        print(n_result)
+
+    dump_json(results, dump_path)
+    print(f"Dump to {dump_path}")
+
+if __name__ == '__main__':
+    # eval_full_func_for_my_data(data_base_path="/data2/zhijietang/vul_data/datasets/joern_vulberta/packed_process_hybrid_data/",
+    #                            file_name_temp="packed_hybrid_vol_{}.pkl",
+    #                            dump_path="/data2/zhijietang/temp/icse2024_intrin/pdbert_intrin_test_full_256_results.json",
+    #                            vol_range=(9999,9999),
+    #                            max_tokens=256)
+    eval_full_func_for_my_data(data_base_path="/data2/zhijietang/vul_data/datasets/docker/fan_dedup/tokenized_packed/",
+                               file_name_temp="packed_hybrid_vol_{}.pkl",
+                               dump_path="/data2/zhijietang/temp/icse2024_intrin/bigvul_full_512_results.json",
+                               vol_range=(0, 1),
+                               max_tokens=512)
